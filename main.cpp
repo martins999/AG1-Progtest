e93554a9175ab1e8cfc5894571fff6ede7a176c0
Martin Sutovsky <matos.utovsky@gmail.com>
2018-10-22 22:48:59 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+class CityDistributionNetwork {
+
+    unsigned int city_count;
+    unsigned int roads_count;
+    unsigned int grocery_count;
+    unsigned int min_grocery_count;
+
+
+
+
+};
+
+
+int main()
+{
+
+
+    return 0;
+}
